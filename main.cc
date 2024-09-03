ffe072957ae68bd9632bd060604ab7acb21ba557
J0sue0805 <00035223@uca.edu.sv>
2024-09-03 16:33:52 -0600

    add structure for person and the event

--- main.cc
+++ main.cc
@@ -1,5 +1,25 @@
 #include <iostream>
 
+const int kCapacity = 10;
+
+struct Person{
+    std:: string name;
+    std:: string lastName;;
+    int age;
+    std:: string media;
+    std:: string interest;
+
+};
+
+struct Event
+{
+    std:: string name;
+    std:: string date;
+    int maxPlaces;
+    std:: string peopleRegistered[kCapacity];
+};
+
+
 int main(int argc, char *argv[]){
     std::cout << "Hello world" << std::endl;
 
